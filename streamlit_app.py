bb305aa62eca2bf1e69a343d2d346ded8b07918b
Franck Mairot <franck.mairot@gmail.com>
2023-06-19 15:26:27 +0200

    Add rotation API and CSV output

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import openai
 from itertools import cycle
+import base64
 
 def get_homepage_content(url):
     if not url.startswith('http://') and not url.startswith('https://'):
@@ -21,19 +22,33 @@ def categorize_site(domain, content, api_key):
     chat = openai.ChatCompletion.create(model="gpt-4", messages=messages)
     return chat.choices[0].message.content
 
+def get_table_download_link(df):
+    csv = df.to_csv(index=False, encoding='utf-8-sig')  # Explicitly encode to UTF-8
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="output.csv">Télécharger le CSV</a>'
+    return href
+
 def main():
     st.title('Scraping et catégorisation des sites web')
 
     csv_file = st.file_uploader('Importez votre CSV', type=['csv'])
-    api_keys = st.text_input('Insérer les API Keys d’OpenAI, séparées par une virgule', type="password")
+    
+    st.subheader('OpenAI API Keys')
+    api_key_1 = st.text_input('Insérer la première clé API OpenAI', type='password')
+    api_key_2 = st.text_input('Insérer la deuxième clé API OpenAI', type='password')
+    api_key_3 = st.text_input('Insérer la troisième clé API OpenAI', type='password')
 
-    if csv_file and api_keys:
+    api_keys = [api_key_1, api_key_2, api_key_3]
+    api_key_cycle = cycle(api_keys)
+
+    if csv_file and all(api_keys):
         data = pd.read_csv(csv_file)
-        api_keys = api_keys.split(',')
-        api_key_cycle = cycle(api_keys)
         data['contenu home page'] = data['domaine du site'].apply(get_homepage_content)
         data['catégorisation du site'] = data.apply(lambda row: categorize_site(row['domaine du site'], row['contenu home page'], next(api_key_cycle)), axis=1)
+
         st.dataframe(data)
+        st.markdown(get_table_download_link(data), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
+
