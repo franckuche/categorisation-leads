d91590d27a71aedabc61179a6aa702cd95f5e442
Franck Mairot <franck.mairot@gmail.com>
2023-06-27 14:59:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import openai
 from itertools import cycle
 import base64
+import time
 
 @st.cache(show_spinner=False)
 def get_homepage_content(url):
@@ -13,7 +14,7 @@ def get_homepage_content(url):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        return soup.get_text(strip=True)[:5000]  # on limite à 5000 caractères
+        return soup.get_text(strip=True)[:5000]
     except:
         return "site n'existe plus"
 
@@ -23,7 +24,6 @@ def categorize_site(domain, homepage_content, api_key):
     openai.api_key = api_key
 
     prompt_text = f"Tu es un spécialiste du marketing de marque. Pour un client, tu dois trouver à partir du texte de la home page et de ta connaissance du nom de marque à quel secteur d’activité il appartient. Tu peux choisir un à deux secteurs d’activité maximum. Si tu en choisi deux, sépare les avec une barre comme celle-ci : / Si tu ne sais pas ou que tu as un doute, mets “Autres”. Tu ne dois pas ajouter de commentaire, de rédaction ou autre, uniquement le ou les secteurs d’activités que tu as choisi. Voici les secteurs d’activité que tu dois choisir : Alimentaire Animaux Art et culture Associations Automobile B2B Banque / Assurance Cosmétique Divertissement Energie Gaming High Tech Home Immobilier Luxe Prêt-à-porter Retail Restauration Sport équipement Travel. Voici le nom du site : {domain}. Voici le texte de la home page : {homepage_content}"
-
     messages = [{"role": "system", "content": prompt_text}]
     chat = openai.ChatCompletion.create(model="gpt-4", messages=messages)
     return chat.choices[0].message.content
@@ -34,13 +34,12 @@ def identify_client_type(homepage_content, api_key):
     openai.api_key = api_key
 
     prompt_text = f"Tu es un expert du domaine du marketing. Je vais te fournir un texte qui est le contenu d’une homepage de site. A partir de celui-ci, je souhaite que tu me choisisse l’un de ces termes : B to B (c’est à dire business to business), B to C (c’est à dire business to customers), B to B to C (c’est à dire des entreprises qui commercialisent des biens et des services auprès de sociétés tierces, qui les revendent elles-mêmes au grand public). Tu peux choisir un seul de ces 3 termes. Tu n’a pas le droit d’ajouter des commentaires, fait juste un choix entre : B to B, B to C et B to B to C. Voici le contenus de la homepage : {homepage_content}"
-
     messages = [{"role": "system", "content": prompt_text}]
     chat = openai.ChatCompletion.create(model="gpt-4", messages=messages)
     return chat.choices[0].message.content
 
 def get_table_download_link(df):
-    csv = df.to_csv(index=False, encoding='utf-8-sig')  # Explicitly encode to UTF-8
+    csv = df.to_csv(index=False, encoding='utf-8-sig')
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="output.csv">Télécharger le CSV</a>'
     return href
@@ -55,7 +54,6 @@ def get_marketing_words(homepage_content, api_key):
     messages = [{"role": "system", "content": prompt_text}]
     chat = openai.ChatCompletion.create(model="gpt-4", messages=messages)
     return chat.choices[0].message.content
-
 
 def main():
     st.title('Scraping et catégorisation des sites web')
@@ -82,7 +80,7 @@ def main():
                 data = pd.read_csv(csv_file, encoding='utf-8')
             except UnicodeDecodeError:
                 try:
-                    data = pd.read_csv(csv_file, encoding='latin-1') # ou une autre encodage qui pourrait être utilisé
+                    data = pd.read_csv(csv_file, encoding='latin-1')
                     data.to_csv(csv_file, encoding='utf-8', index=False)
                     data = pd.read_csv(csv_file, encoding='utf-8')
                 except Exception as e:
@@ -93,10 +91,19 @@ def main():
             domains_data = pd.DataFrame(domains, columns=['domaine du site'])
             data = pd.concat([data, domains_data], ignore_index=True)
 
-        data['contenu home page'] = data['domaine du site'].apply(get_homepage_content)
-        data['catégorisation du site'] = data.apply(lambda row: categorize_site(row['domaine du site'], row['contenu home page'], next(api_key_cycle)), axis=1)
-        data['Business target'] = data['contenu home page'].apply(lambda x: identify_client_type(x, next(api_key_cycle)))
-        data['Marketing words'] = data['contenu home page'].apply(lambda x: get_marketing_words(x, next(api_key_cycle)))
+        for i in range(0, len(data), 6):  
+            batch = data.iloc[i:i+6]
+            batch['contenu home page'] = batch['domaine du site'].apply(get_homepage_content)
+            batch['catégorisation du site'] = batch.apply(lambda row: categorize_site(row['domaine du site'], row['contenu home page'], next(api_key_cycle)), axis=1)
+            batch['Business target'] = batch['contenu home page'].apply(lambda x: identify_client_type(x, next(api_key_cycle)))
+            batch['Marketing words'] = batch['contenu home page'].apply(lambda x: get_marketing_words(x, next(api_key_cycle)))
+
+            if i == 0:
+                data = batch
+            else:
+                data = pd.concat([data, batch])
+
+            time.sleep(60)  # Attend 60 secondes
 
         st.dataframe(data)
         st.markdown(get_table_download_link(data), unsafe_allow_html=True)
